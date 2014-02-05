b642ea3048670dbb0aa007c7792e8bb5e8933488
Emmanuel Garcia <garciadece1@nku.edu>
2014-02-05 15:21:41 -0500

    has_secure_password module imported in the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,16 @@
 class User < ActiveRecord::Base
+  # Includes ActiveModel::SecurePassword
+  # Adds methods to set and authenticate against a BCrypt password. 
+  # This mechanism requires you to have a password_digest attribute.
+
+  # Validations for presence of password on create, confirmation of 
+  # password (using a password_confirmation attribute) are automatically added. 
+  # If you wish to turn off validations, pass validations: false as an argument. 
+  # You can add more validations by hand if need be.
+  
+  has_secure_password
+
+  # Model validations
   validates :name, presence: true, length: { minimum: 2 }
   validates :nickname, presence: true, length: { minimum: 2 }
   validates :email, presence: true, format: { with: /\A[a-zA-Z0-9_\.-]{1,}@[a-zA-Z0-9_\.-]{1,}\.[a-zA-Z0-9_-]{2,}\z/, message: "Invalid email format" }

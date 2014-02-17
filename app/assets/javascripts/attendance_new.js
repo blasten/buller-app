ba7a290e07d5d7a80c79419ad34caa6f3c6bf0b4
Kevin Rockwood <kevin@rockwood.me>
2014-02-16 22:02:58 -0500

    Simplify selectSeat function
    In this case, it's probably not worth the complexity to allow deselecting a seat since an attendance isn't valid without one.

--- app/assets/javascripts/attendance_new.js
+++ app/assets/javascripts/attendance_new.js
@@ -1,29 +1,14 @@
 (function(window) {
-  function selectQuadrant($quadrant) {
-    var index = $quadrant.index() + 1;
-    // Remove the class of the current selection
-    $(".seat-selected").removeClass("seat-selected");
-    $quadrant.toggleClass("seat-selected");
-
-    if ($quadrant.hasClass("seat-selected")) {
-      $("#attendance_seat").attr("value", index);
-      $("#profile-picture").show().appendTo($quadrant);
-    } else {
-      $("#attendance_seat").attr("value", "");
-      $("#profile-picture").hide();
-    }
+  function selectSeat($seat) {
+    $(".seats .seat").removeClass("seat-selected");
+    $seat.addClass("seat-selected");
+    $("#attendance_seat").attr("value", $seat.index() + 1);
   }
 
-  // Trigger on DOM Ready 
+  // Trigger on DOM Ready
   $(function() {
-    $(".seats").on("click", ".seat", function(event) {
-      selectQuadrant($(event.currentTarget));
+    $(".seats .seat").on("click", function(event) {
+      selectSeat($(this));
     });
-
-    // Read the current seat and update the DOM
-    var currentQuadrant = $("#attendance_seat").attr("value");
-    if (currentQuadrant!=="") {
-      selectQuadrant($(".seats").children(":nth-child(" + currentQuadrant + ")"));
-    }
   });
 })(this);

d0a8e2519ba7dfd350b6cf31686484899a0a5156
Emmanuel Garcia <garciadece1@nku.edu>
2014-03-07 21:28:14 -0500

    Define an active_tab method to change the active tab in the navigation bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def title(page_title)
     content_for(:title) { page_title }
   end
+
+  def active_tab(*args)
+    if args.length == 1
+       @active_tab = args[0]
+    else
+      @active_tab || nil
+    end
+  end
 end

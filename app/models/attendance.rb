622ae4e5a382edb34bcd7603b4e0ad457287184c
Emmanuel Garcia <garciadece1@nku.edu>
2014-03-20 16:54:27 -0400

    Set default values for `attended_on` and `user`

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -2,4 +2,7 @@ class Attendance < ActiveRecord::Base
   belongs_to :user
   validates :seat, numericality: true, presence: true
 
+  # Set default values for `attended_on` and `user`
+  before_save{ |attendance| attendance.attended_on ||= Date.today }
+  before_save{ |attendance| attendance.user ||= defined?(current_user) ? current_user : nil }
 end
